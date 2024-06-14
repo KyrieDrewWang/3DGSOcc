7fd05780c85816d87bbb5bbf420f62ddb16a6207
wang cheng <wangchengkyrie@outlook.com>
2024-06-14 14:12:28 +0800

    one frame, semantic rendering only, all the other components are left out, pure and latest version

--- socc_8gpu.sh
+++ socc_8gpu.sh
@@ -39,7 +39,7 @@ BATCH_JOB_ID=$5
 echo "$NODE_RANK,$NODES,$NPROC_PER_NODE,$MASTER_ADDR,$BATCH_JOB_ID"
 OUTPUT_LOG="train_rank${NODE_RANK}_${BATCH_JOB_ID}.log"
 
-WORK_DIR="socc_1_8"
+WORK_DIR="socc_1_12"
 
 torchrun \
      --nnodes="${NODES}" \
